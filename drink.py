a12ea512abebc141a8ff862d956b6c1f96ce662e
Charlie McClatchy <charliemcclatchy@gmail.com>
2020-11-07 09:40:43 -0500

    Drink has been changed to an object to make accessing price easier. This comes with the addition of a size parameter for drinks. Both pizza and drink have the ability to read their prices from a json file. Tests have been added for every method in both drink and pizza.

---
+++ drink.py
@@ -0,0 +1,60 @@
+import json
+
+class Drink:
+    'Drinks and their relevant data are defined here'
+    price = None
+
+    def __init__(self, drink_type, size):
+        with open('data.json') as f:
+            self.data = json.load(f)
+        if self.check_inputs(drink_type, size):
+            self.drink_type = drink_type
+            self.size = size
+            self.price = self.get_price()
+
+    def get_size(self):
+        'Returns the drink size'
+        return self.size
+
+    def get_type(self):
+        'Returns the drink type'
+        return self.drink_type
+
+    def get_price(self):
+        """Returns the drink price and reads price from data file if price 
+        is not initialized"""
+        if self.price is None:
+            return self.data['drink'][self.drink_type][self.size]
+        else:
+            return self.price
+
+    def manual_price_update(self, price):
+        'Manually updates and returns price for this drink instance'
+        self.price = price
+        return self.get_price()
+
+    def check_inputs(self, drink_type, size):
+        """Returns boolean according to if there exists a valid drink under the 
+        given inputs"""
+        try:
+            self.data['drink'][drink_type]
+            try: 
+                self.data['drink'][drink_type][size]
+                return True
+            except KeyError:
+                print('Error: Drink size does not exist for ' + drink_type + '.')
+                return False
+        except KeyError:
+            print('Error: Drink type does not exist.')
+            return False
+
+    def update(self, drink_type, size):
+        """Updates drink type and/or size according to parameters 
+        and checks validity"""
+        if drink_type == -1:
+            drink_type = self.drink_type
+        if size == -1:
+            size = self.size
+        if self.check_inputs(drink_type, size):
+            self.drink_type = drink_type
+            self.size = size
--- pizza.py
+++ pizza.py
@@ -1,15 +1,67 @@
+import json
+
 class Pizza:
     'Pizzas and their relevant data are defined here'
+    price = None
 
-    def __init__(self, size, p_type, toppings):
-        self.size = size
-        self.p_type = p_type
-        self.toppings = toppings
-
-    def update(self, size, p_type, toppings):
-        if size != -1:
+    def __init__(self, p_type, size, toppings):
+        with open('data.json') as f:
+            self.data = json.load(f)
+        if self.check_inputs(p_type, size, toppings):
             self.size = size
-        if p_type != -1:
             self.p_type = p_type
-        if toppings != -1:
             self.toppings = toppings
+            self.price = self.get_price()
+
+    def get_size(self):
+        return self.size
+
+    def get_type(self):
+        return self.p_type
+
+    def get_toppings(self):
+        return self.toppings
+
+    def get_price(self):
+        if self.price is None:
+            price = self.data['pizza'][self.p_type][self.size]
+            for topping in self.toppings:
+                price += self.data['topping'][topping]
+            return price
+        return self.price
+
+    def manual_price_update(self, price):
+        self.price = price
+        return self.get_price()
+
+    def check_inputs(self, p_type, size, toppings):
+        try:
+            self.data['pizza'][p_type]
+            try:
+                self.data['pizza'][p_type][size]
+                for topping in toppings:
+                    try:
+                        self.data['topping'][topping]
+                    except KeyError:
+                        print('Error: Pizza topping ' + topping + ' does not exist.')
+                        return False
+                return True
+            except KeyError:
+                print('Error: ' + size + ' size does not exist for ' + p_type + ' pizza.')
+                return False
+        except KeyError:
+            print('Error: ' + p_type + ' pizza does not exist.')
+            return False
+
+    def update(self, p_type, size, toppings):
+        if size == -1:
+            size = self.size
+        if p_type == -1:
+            p_type = self.p_type
+        if toppings == -1:
+            toppings = self.toppings
+        if self.check_inputs(p_type, size, toppings):
+            self.p_type = p_type
+            self.size = size
+            self.toppings = toppings
+
