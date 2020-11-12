260a81cba39e15009e2f6270cccc0dbbbcd0d53f
Owen Hu <owenhu99@gmail.com>
2020-11-11 21:22:52 -0500

    Add API routing for creating and getting orders

--- pizza_parlour.py
+++ pizza_parlour.py
@@ -14,9 +14,66 @@ def index():
     return 'Welcome to the Pizza Parlour'
 
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(err):
     """404 Response"""
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+@app.errorhandler(400)
+def bad_request(err):
+    """400 Response"""
+    return "<h1>400</h1><p>Bad Request.</p>", 400
+
+def no_content_found():
+    """204 Response"""
+    return "<h1>204</h1><p>No content found.</p>", 204
+
+@app.route('/v1/orders/<int:order_number>', methods=['GET'])
+def api_get_order(order_number):
+    """Get order by order number"""
+    order = orderManager.get_order(order_number)
+    if order:
+        return jsonify({
+            'order_number': order_number,
+            'pizzas': [pizza.get_dict() for pizza in order.get_pizzas()],
+            'drinks': [drink.get_dict() for drink in order.get_drinks()],
+            'total': order.get_price()
+        })
+    print("Error: No order found with the order number: " + str(order_number))
+    return no_content_found()
+
+@app.route('/v1/orders', methods=['POST'])
+def api_create_order():
+    """Create an order
+    json format:
+    {
+        'pizzas': [
+            {'type': '<type>", 'size': '<size>', 'toppings': '<topping1>, <topping2>, ...'},
+            {'type': '<type>", 'size': '<size>', 'toppings': '<topping1>, <topping2>, ...'},
+            ...
+        ],
+        'drinks': [
+            {'type': '<type>', 'size': '<size>'},
+            {'type': '<type>', 'size': '<size>'},
+            ...
+        ]
+    }
+    """
+    if not request.json or not 'pizzas' in request.json or not 'drinks' in request.json:
+        return bad_request(400)
+    pizzas, drinks = [], []
+    for pizza in request.json['pizzas']:
+        pizzas.append([
+            pizza['type'],
+            pizza['size'],
+            [x.strip() for x in pizza['toppings'].split()]
+        ])
+    for drink in request.json['drinks']:
+        drinks.append([
+            drink['type'],
+            drink['size']
+        ])
+    order_num = orderManager.order(pizzas, drinks)
+    return jsonify({'order_number': order_num})
 
 @app.route('/v1/resources/menu/all', methods=['GET'])
 def api_all():
@@ -37,15 +94,15 @@ def api_item():
             result = data[itype][item]
         except KeyError as err:
             print("KeyError: {0}".format(err))
-            return "<h1>204</h1><p>No content found.</p>", 204
+            return no_content_found()
     elif itype and item and size:
         try:
             result = data[itype][item][size]
         except KeyError as err:
             print("KeyError: {0}".format(err))
-            return "<h1>204</h1><p>No content found.</p>", 204
+            return no_content_found()
     else:
-        return page_not_found()
+        return page_not_found(404)
 
     return jsonify(price=result)
 
